0b5d2eb1dfa8d26166d94ec768abec96ffd8f456
k4keye <vps32@naver.com>
2020-07-27 15:00:54 +0900

    [projectAdd]ImageAlerm

---
+++ ImageAlerm/ImageAlerm/ImageAlerm.Android/Camera_android.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Provider;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace ImageAlerm.Droid
+{
+    
+    class Camera_android
+    {
+
+    }
+}
